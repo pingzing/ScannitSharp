baeaa3e6c7a12807a0cc8505c108ecfab1f35426
Neil McAlister <neil.mcalister@futurice.com>
2019-07-18 16:30:06 +0300

    Initial commit. DLLs just sort of stuffed in here, we'll fix that later.

---
+++ ScannitSharp.Bindings/RustString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ScannitSharp.Bindings
+{
+    internal class RustString : SafeHandle
+    {
+        private string _cSharpString;
+
+        internal RustString(IntPtr ptr) : base(IntPtr.Zero, true)
+        {
+            this.handle = ptr;
+        }
+
+        internal RustString() : base(IntPtr.Zero, true) { }
+
+        public override bool IsInvalid => false;
+        protected override bool ReleaseHandle()
+        {
+            Native.free_string(handle);
+            return true;
+        }
+
+        internal string AsCSharpString()
+        {
+            int len = 0;
+            while (Marshal.ReadByte(handle, len) != 0)
+            {
+                ++len;
+            }
+
+            byte[] buffer = new byte[len];
+            Marshal.Copy(handle, buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        public override string ToString()
+        {
+            if (_cSharpString == null)
+            {
+                _cSharpString = AsCSharpString();
+            }
+
+            return _cSharpString;
+        }
+    }
+}
---
+++ ScannitSharp.CliExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using ScannitSharp.Bindings;
+using System;
+
+namespace ScannitSharp.CliExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Native.GetString());
+
+            var vector = Native.get_vector();
+            vector.GetData();
+            Native.free_vector(vector);
+        }
+    }
+}
