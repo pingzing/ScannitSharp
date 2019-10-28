5975bc5e69584fe0b61d6f075bf00f253bda06d1
Neil McAlister <neil.mcalister@futurice.com>
2019-10-28 17:12:50 +0200

    Update updatey-script

--- ScannitSharp/Get-LatestNativeDeps.ps1
+++ ScannitSharp/Get-LatestNativeDeps.ps1
@@ -1,22 +1,23 @@
 $fileNameToFolderName = @{
+    # Zipfile name = ("destination folder name", "library file name inside zip file")
     #Androids
-    "aarch64-linux-android"    = "android-arm64";
-    "arm-linux-androideabi"    = "android-arm";
+    "aarch64-linux-android"    = @("android-arm64", "libscannit_core_ffi.so");
+    "arm-linux-androideabi"    = @("android-arm", "libscannit_core_ffi.so");
     #Windowses
-    "x86_64-pc-windows-msvc"   = "win-x64";
-    "i686-pc-windows-msvc"     = "win-x86";
+    "x86_64-pc-windows-msvc"   = @("win-x64", "scannit_core_ffi.dll");
+    "i686-pc-windows-msvc"     = @("win-x86", "scannit_core_ffi.dll");
     #OSX
-    "x86_64-apple-darwin"      = "osx-64";
+    "x86_64-apple-darwin"      = @("osx-x64", "libscannit_core_ffi.dylib");
     #Linuxes
-    "x86_64-unknown-linux-gnu" = "linux-x64";
-    "i686-unknown-linux-gnu"   = "linux-x86";
+    "x86_64-unknown-linux-gnu" = @("linux-x64", "libscannit_core_ffi.so");
+    "i686-unknown-linux-gnu"   = @("linux-x86", "libscannit_core_ffi.so");
 };
 
 # Grab the latest release from GitHub, and stuff its various .dlls and .sos and dylibs into the right places.
 # Also, update the version number in the .csproj.
 
 # Get latest release from GitHub:
-$scratchFolder = New-Item -ItemType Directory "_latestDeps"
+$scratchFolder = New-Item -ItemType Directory "_latestDeps" -Force
 $githubResponse = Invoke-RestMethod https://api.github.com/repos/pingzing/scannit-core/releases/latest;
 if (-not($githubResponse)) {
     Write-Error "Failed to get a valid response from GitHub. Bailing.";
@@ -26,8 +27,38 @@ if (-not($githubResponse)) {
 $versionString = $githubResponse.tag_name.Substring(1);
 
 foreach ($asset in $githubResponse.assets) {
-    $result = Invoke-WebRequest $asset.browser_download_url -OutFile "$($scratchFolder.FullName)/$($asset.name)";
-    if ( -not($result -AND $result.StatusCode -eq 200 )) {
-        Write-Error "Failed to download $($asset.name)";
+    $outFile = "$($scratchFolder.FullName)/$($asset.name)";
+    Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $outFile; # This never returns anything to inspect. What the hell.
+    # Get the parts filename that'll map to the hashtable up above. That's everything after
+    # 'scannit-core_versionNumber_' and before '.zip'.
+    [string]$zipFileName = $asset.name;
+    [int]$firstUnderscoreIndex = $zipFileName.IndexOf('_');
+    [int]$secondUnderscoreIndex = $zipFileName.Substring($firstUnderscoreIndex + 1).IndexOf('_');
+    [int]$beginIndex = $firstUnderscoreIndex + $secondUnderscoreIndex + 2;
+    [int]$lengthToSubstring = $zipFileName.Length - ($zipFileName.Length - $zipFileName.IndexOf('.')) - $beginIndex;
+    [string]$sanitizedFileName = $zipFileName.Substring($beginIndex, $lengthToSubstring);
+    [string]$nativeFolderName = $fileNameToFolderName[$sanitizedFileName][0];
+    [string]$fileNameToExtract = $fileNameToFolderName[$sanitizedFileName][1];
+    [string]$nativeFileExtension = $fileNameToExtract.Substring($fileNameToExtract.IndexOf('.'));
+    
+    # This is (supposed to be) idempotent.
+    Add-Type -Assembly System.IO.Compression.FileSystem;
+    [System.IO.Compression.ZipArchive]$zipFile;
+    [string]$destPath = "$($PSScriptRoot)/runtimes/$($nativeFolderName)/native/scannit_core_ffi$($nativeFileExtension)";
+    try {
+        $zipFile = [IO.Compression.ZipFile]::OpenRead("$($scratchFolder.FullName)/$($zipFileName)");
+        $zipFile.Entries | 
+            Where-Object {$_.Name -eq $fileNameToExtract} | 
+            ForEach-Object{ 
+                [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $destPath, $true);
+            };
+    } catch {
+        Write-Error("Failed to open the zip file: $($_.Exception.Message)");
+    }
+    finally {
+        $zipFile.Dispose();
+        Write-Host "Extracted file to $destPath";
     }
 }
+
+# TODO: Update csproj version number with whatever we pulled out of GitHub.
