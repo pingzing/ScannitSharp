4ee71b57b0da5883864683eb60072154410d1eb3
Neil McAlister <neil.mcalister@futurice.com>
2019-11-01 15:30:17 +0200

    Make local debugging a bit easier

--- ScannitSharp/Native.cs
+++ ScannitSharp/Native.cs
@@ -5,7 +5,14 @@ namespace ScannitSharp
 {
     internal static class Native
     {
-        [DllImport("scannit_core_ffi")]
+#if DEBUG
+        // Or rename this to whatever plat/arch you're debugging on.
+        const string ScannitDllPath = "runtimes/win-x64/native/scannit_core_ffi";
+#else
+        const string ScannitDllPath = "scannit_core_ffi";
+#endif
+
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr create_travel_card(
             IntPtr app_info_ptr, UIntPtr app_info_size,
             IntPtr control_info_ptr, UIntPtr control_info_size,
@@ -14,46 +21,46 @@ namespace ScannitSharp
             IntPtr e_ticket_ptr, UIntPtr e_ticket_size,
             IntPtr history_ptr, UIntPtr history_size);
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern void free_travel_card(IntPtr travel_card_ptr);
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_GET_VERSION_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_GET_APPLICATION_IDS_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_SELECT_HSL_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_APP_INFO_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_CONTROL_INFO_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_PERIOD_PASS_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_STORED_VALUE_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_E_TICKET_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_HISTORY_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_READ_NEXT_COMMAND();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_OK_RESPONSE();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_ERROR_RESPONSE();
 
-        [DllImport("scannit_core_ffi")]
+        [DllImport(ScannitDllPath)]
         internal static extern IntPtr get_MORE_DATA_RESPONSE();
     }
 }
